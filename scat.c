d580811db3ab9802bedc9b03fb83771b593726f8
Jakub Wilk <jwilk@jwilk.net>
2008-11-08 17:59:24 +0100

    Update copyright information.

--- scat.c
+++ scat.c
@@ -1,7 +1,15 @@
-/* Copyright (C) 2001, ..., 2005 Jakub Wilk
+/* Copyright © 2001, 2002, 2004, 2005, 2008
+ * Jakub Wilk <ubanus@users.sf.net>.
+ *
  * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 as published 
- * by the Free Software Foundation.
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
  */
 
 #include <errno.h>
