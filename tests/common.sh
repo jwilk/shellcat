b14011630880dcc112545acde00cf822fd925dce
Jakub Wilk <jwilk@jwilk.net>
2013-01-19 20:51:34 +0100

    Add tests framework.

---
+++ tests/common.sh
@@ -0,0 +1,13 @@
+shellcat=${shellcat:-../shellcat}
+base="${0}"
+f_input="${base}.in"
+f_expected="${base}.exp"
+f_output="${base}.out"
+
+run_shellcat()
+{
+    "$shellcat" "$f_input" > "$f_output"
+    exec diff -u "$f_expected" "$f_output" 
+}
+
+# vim:ts=4 sw=4 et
