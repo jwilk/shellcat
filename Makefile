jwilk/shellcat

commit 2cc38b2d34eba42bb568dfc272f6610b551b18d0

Enable LFS support.

Makefile
@@ -23,7 +23,10 @@ version = $(shell head -n1 doc/changelog | cut -d ' ' -f2 | tr -d '()')
 CC ?= gcc
 CFLAGS ?= -g -O2
 CFLAGS += -Wall -Wformat -Wextra -pedantic
+CFLAGS += $(shell getconf LFS_CFLAGS)
 CPPFLAGS += -DVERSION='"$(version)"'
+LDFLAGS += $(shell getconf LFS_LDFLAGS)
+LDLIBS += $(shell getconf LFS_LIBS)
 
 PREFIX = /usr/local
 DESTDIR =

tests/test-lfs.exp
@@ -1,0 +1,1 @@
+OK

tests/test-lfs.t
@@ -1,0 +1,9 @@
+#!/bin/sh
+. ./common.sh
+
+f_input="${base}.tmp"
+printf 'OK\n<$ exit 0 $>' > "$f_input"
+truncate -s 4G "$f_input"
+run_shellcat || [ $? -eq 141 ] || exit 1
+
+# vim:ft=sh ts=4 sts=4 sw=4 et

commit c554403766e6d6e15ce8b5d664ed5b96b67b684f

Don't use getconf(1) to acquire LFS build flags.
The use of getconf(1) doesn't play well with cross-compiling.

Set _FILE_OFFSET_BITS=64 directly, which should be good enough for all
practical purposes.

Fixes: 2cc38b2d34eba42bb568dfc272f6610b551b18d0

Makefile
@@ -23,10 +23,8 @@ version = $(shell head -n1 doc/changelog | cut -d ' ' -f2 | tr -d '()')
 CC ?= gcc
 CFLAGS ?= -g -O2
 CFLAGS += -Wall -Wformat -Wextra -pedantic
-CFLAGS += $(shell getconf LFS_CFLAGS)
+CFLAGS += -D_FILE_OFFSET_BITS=64
 CPPFLAGS += -DVERSION='"$(version)"'
-LDFLAGS += $(shell getconf LFS_LDFLAGS)
-LDLIBS += $(shell getconf LFS_LIBS)
 
 PREFIX = /usr/local
 DESTDIR =