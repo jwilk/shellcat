2b6860c1dd7111fc11f95613026c1d99f754fd19
Jakub Wilk <jwilk@jwilk.net>
2019-01-18 12:42:34 +0100

    Reduce indentation in help message.

--- shellcat.c
+++ shellcat.c
@@ -68,9 +68,9 @@ static void show_usage(FILE *fp)
     fprintf(fp,
         "Usage: %s [options] FILE [ARGUMENT...]\n\n"
         "Options:\n"
-        "  -s, --shell=SHELL   change the default shell to SHELL\n"
-        "  -h, --help          show this help message and exit\n"
-        "  -v, --version       show version information and exit\n\n",
+        "  -s, --shell=SHELL  change the default shell to SHELL\n"
+        "  -h, --help         show this help message and exit\n"
+        "  -v, --version      show version information and exit\n\n",
         progname
     );
 }
