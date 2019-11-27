jwilk/shellcat

commit 6218105575999df717806104138bb8110934b62d

t: port test suite to TAP.

Makefile
@@ -49,7 +49,7 @@ endif
 
 .PHONY: test check
 test check: shellcat
-	$(MAKE) -C tests
+	prove -v
 
 .PHONY: clean
 clean:

tests/Makefile
@@ -1,34 +1,0 @@
-# Copyright © 2013-2018 Jakub Wilk <jwilk@jwilk.net>
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the “Software”), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-.PHONY: all
-all: ../shellcat
-	./run-tests $(<)
-
-../shellcat:
-	$(MAKE) -C ..
-
-.PHONY: clean
-clean:
-	rm -rf *.err *.out *.report *.tmp
-
-.error = GNU make is required
-
-# vim:ts=4 sts=4 sw=4 noet

t/common.sh
@@ -1,0 +1,55 @@
+set -e -u
+
+shellcat=${shellcat:-${0%/*}/../shellcat}
+shellcat_ifs=${shellcat_ifs:-$IFS}
+ifs=$IFS
+base="${0%.t}"
+f_input="${base}.in"
+f_expected="${base}.xout"
+f_exp_err="${base}.xerr"
+[ -e "$f_exp_err" ] || f_exp_err=/dev/null
+f_output="${base}.out"
+f_errors="${base}.err"
+
+diag()
+{
+    sed -e 's/^/# /' <<EOF
+$@
+EOF
+}
+
+run_shellcat()
+{
+    echo 1..1
+    IFS="$shellcat_ifs"
+    if [ $# = 0 ]
+    then
+        xrc=0
+    else
+        xrc=$1
+        shift
+    fi
+    set -- $shellcat "$f_input" "$@"
+    IFS="$ifs"
+    rc=0
+    "$@" > "$f_output" 2>"$f_errors" || rc=$?
+    outdiff=$(diff -u "$f_expected" "$f_output") || true
+    errdiff=$(diff -u "$f_exp_err" "$f_errors") || true
+    if [ -n "$errdiff" ]
+    then
+        echo not ok 1 unexpected stderr
+        diag "$errdiff"
+    elif [ -n "$outdiff" ]
+    then
+        echo not ok 1 unexpected stdout
+        diag "$outdiff"
+    elif [ $rc != $xrc ]
+    then
+        echo not ok 1 "\$? = $rc, expected $xrc)"
+    else
+        echo ok 1
+    fi
+    rm "$f_output" "$f_errors"
+}
+
+# vim:ts=4 sts=4 sw=4 et

t/generic-test
@@ -1,5 +1,5 @@
 #!/bin/sh
-. ./common.sh
+. ${0%/*}/common.sh
 
 run_shellcat
 

t/test-all-characters.t
@@ -1,9 +1,10 @@
 #!/bin/sh
-. ./common.sh
+. ${0%/*}/common.sh
 
 f_input="${base}.tmp"
 f_expected="${base}.tmp"
 perl -e 'use List::Util qw(shuffle); srand 0; print map(chr, shuffle 0..0xFF)' > "$f_input"
 run_shellcat
+rm "$f_input"
 
 # vim:ft=sh ts=4 sts=4 sw=4 et

t/test-arguments.t
@@ -1,7 +1,7 @@
 #!/bin/sh
-. ./common.sh
+. ${0%/*}/common.sh
 
-run_shellcat eggs "'bacon'" '
+run_shellcat 0 eggs "'bacon'" '
 s
 p
 a

commit 6f9ccc630932554c6086ac13e60292b8e54575b6

Makefile: fix cleaning.
Fixes:

    $ make clean
    rm -f shellcat
    make -C tests clean
    make[1]: *** tests: No such file or directory.  Stop.
    make: *** [Makefile:61: clean] Error 2

Fixes: commit 6218105575999df717806104138bb8110934b62d

Makefile
@@ -58,7 +58,7 @@ test-installed: $(or $(shell command -v shellcat;),$(bindir)/shellcat)
 .PHONY: clean
 clean:
 	rm -f shellcat
-	$(MAKE) -C tests clean
+	cd t && rm -f *.err *.out *.tmp
 
 .error = GNU make is required
 